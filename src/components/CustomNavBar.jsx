671d4f02f00bee6ca3aa99ea4de0e5543d9bcbc6
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-09-29 10:04:05 -0400

    working navbar with href update

--- src/components/CustomNavBar.jsx
+++ src/components/CustomNavBar.jsx
@@ -15,13 +15,13 @@ return (
     </Navbar.Header>
     <Navbar.Collapse>
     <Nav pullRight>
-      <NavItem eventKey={1} componentClass={Link} to="/">
+      <NavItem eventKey={1} componentClass={Link} href="/" to="/">
       Home
       </NavItem>
-      <NavItem eventKey={2} componentClass={Link} to="/about">
+      <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
       About
       </NavItem>
-      <NavItem eventKey={3} componentClass={Link} to="/news">
+      <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
       News
       </NavItem>
       </Nav>
