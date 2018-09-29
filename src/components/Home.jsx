ca39142e92d61c630d380d63183b9964cfb4bcbf
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-09-29 08:27:03 -0400

    working implement of bootstrap

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import './Home.css';
+
+export default class Home extends Component {
+render() {
+return (
+  <Grid>
+    <Jumbotron>
+    <h2> Welcome to Outback Tracks </h2>
+    <p> Your tracks destination </p>
+    </Jumbotron>
+    <Link to="/about">
+    <Button> bsStyle="primary">Recent Tracks</Button>
+    </Link>
+  </Grid>
+    )
+  }
+}
---
+++ src/components/News.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import './Home.css';
+
+export default class News extends Component {
+render() {
+return (
+  <div>
+  News Page
+  </div>
+    )
+  }
+}
